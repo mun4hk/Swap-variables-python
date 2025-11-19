cc0c970988c8420fad5f8b520e3c31661070bf00
mun4hk <242577074+mun4hk@users.noreply.github.com>
2025-11-19 13:51:09 +0300

    Use tuple unpacking for swap

--- main.py
+++ main.py
@@ -5,19 +5,8 @@ something_2 = input("Введите ещё что-нибудь: ")
 #Проверка ввода
 print(f"Вы ввели это: '{something_1}' и это: '{something_2}'\n")
 
-
-#Сохранение в новых переменных
-safe_something_1 = something_1
-safe_something_2 = something_2
-
-#Проверка сохранения данных
-print(f"В переменной <safe_something_1> сохранено это: '{safe_something_1}'")
-print(f"В переменной <safe_something_2> сохранено это: '{safe_something_2}'\n")
-
-
 #Обмен значениями
-something_1 = safe_something_2
-something_2 = safe_something_1
+something_1, something_2 = something_2, something_1
 
 #Проверка обмена значениями
 print(f"Переменная <something_1> содержит: '{something_1}'")
